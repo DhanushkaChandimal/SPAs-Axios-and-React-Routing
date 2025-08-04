7775e665e0b57e27eb52a62723c0366723626b08
Dhanushka <dhanushkamcr@gmail.com>
2025-08-04 06:15:57 -0500

    Implemented NotFound component

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import HomePage from './components/HomePage';
 import Users from './components/Users';
 import UserTodos from './components/UserTodos';
 import NavBar from './components/NavBar';
+import NotFound from './components/NotFound';
 
 function App() {
 
@@ -17,6 +18,7 @@ function App() {
         <Route path="/" element={<HomePage />} />
         <Route path="/users" element={<Users />} />
         <Route path="/user-todos/:userId" element={<UserTodos />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </>
   )
